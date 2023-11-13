57f4b3a59132d5453671ab3df9fb5725d7bc89c6
Under Penalty <underpenalty@gmail.com>
2023-11-13 14:34:54 -0500

    Adding base layout for menu/website

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import ResponsiveAppBar from '@/components/app-bar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Robert Michael Pollum',
+  description: 'Self Hosted Personal Developer Profile',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ResponsiveAppBar />
+        {children}</body>
+    </html>
+  )
+}
