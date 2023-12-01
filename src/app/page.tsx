5a71609c3d8b4a6a1b106dba92f23031438ee040
Under Penalty <underpenalty@gmail.com>
2023-12-01 00:11:08 -0500

    Save status of add jsFiddle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import PersonalLink from '@/components/personal-link'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+        <PersonalLink icon={<GitHubIcon/>} link='https://github.com/UnderPenalty' displayName='Under Penalty'/>
+        <PersonalLink icon={<LinkedInIcon/>} link='https://www.linkedin.com/in/robert-pollum-aa88b6b2/' displayName='Robert Pollum '/>
       </div>
     </main>
   )
---
+++ src/components/personal-link.tsx
@@ -0,0 +1,20 @@
+import Grid from "@mui/material/Grid"
+import Link from "next/link"
+
+interface PersonalComponentProps {
+    icon: React.ReactNode,
+    link: string,
+    displayName: string
+}
+export default function PersonalLink(props: PersonalComponentProps) {
+    const {icon, link, displayName} = props;
+    return (
+    <Grid container>
+        <Grid item xs={4}>
+            {icon}
+        </Grid>
+        <Grid item xs={8}>
+            <Link href={link}>{displayName}</Link>
+        </Grid>
+    </Grid>)
+}
